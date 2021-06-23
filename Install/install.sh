889206d5cee128753bcc955083ab91d2461fa204
chparmley <63277749+chparmley@users.noreply.github.com>
2021-06-23 01:27:49 +0000

    Initial Upload

---
+++ Install/install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+project_name="spuncode"
+website_name="spuncode.com"
+
+
+
+user_name=$(whoami)
+cd ..
+project_folder="${PWD##*/}"
+cd Install
+
+# Configuring setup files
+sed -i "s/user_name/$user_name/g" ./config/*
+sed -i "s/project_name/$project_name/g" ./config/*
+sed -i "s/website_name/$website_name/g" ./config/*
+sed -i "s/project_folder/$project_folder/g" ./config/*
+
+sudo apt install python3-pip python3-dev libpq-dev nginx curl python3-venv -y
+
+cd ..
+python3 -m venv ./venv
+source ./venv/bin/activate
+pip install django gunicorn psycopg2-binary
+deactivate
+
+sudo cp ./Install/sock_file /etc/systemd/system/gunicorn.socket
+sudo cp ./Install/service_file /etc/systemd/system/gunicorn.service
+
+sudo systemctl start gunicorn.socket
+sudo systemctl enable gunicorn.socket
+
+sudo cp ./Install/nginx_config /etc/nginx/sites-available/$project_name
+sudo ln -s /etc/nginx/sites-available/$project_name /etc/nginx/sites-enabled
+
+sudo systemctl restart gunicorn
+sudo systemctl daemon-reload
+
+sudo systemctl restart nginx
+
